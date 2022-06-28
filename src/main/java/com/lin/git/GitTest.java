1d4df6c12f989644d878fefca9cb93f26b552ca7
lin <2915069270@qq.com>
2022-06-28 18:53:43 +0800

    idea first commit

---
+++ src/main/java/com/lin/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.lin.git;
+
+/**
+ * @author shkstart
+ * @create 2022-06-28 17:46
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
